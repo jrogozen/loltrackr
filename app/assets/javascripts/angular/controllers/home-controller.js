64a5f0025f59caa27968d42cf996f3dafae32ca0
Jon Rogozen <jon.rogozen@gmail.com>
2014-11-03 08:46:13 -0800

    heroku;

--- app/assets/javascripts/angular/controllers/home-controller.js
+++ app/assets/javascripts/angular/controllers/home-controller.js
@@ -1,6 +1,6 @@
 app.controller('HomeCntrl', ['$scope', '$rootScope', '$location', '$routeParams', 'Video', 'RiotApi', 'Search', function($scope, $rootScope, $location, $routeParams, Video, RiotApi, Search) {
 
-    $scope.champions = RiotApi.getChampions.query();
+    $scope.champions = RiotApi.getChampionsOnly.query();
 
     $scope.championImage = function(champImage) {
       return RiotApi.championImage(champImage);
